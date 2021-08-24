4b3d64633e2668473d5b0787733a023f6928388c
Lakmal Rathnasiri <lakmal@calcey.com>
2021-08-24 12:42:27 +0530

    CollectLauncherActivity login flow

--- collect_app/src/main/java/org/odk/collect/android/activities/CollectLauncherActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/CollectLauncherActivity.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 
 import org.odk.collect.android.R;
+import org.odk.collect.android.activities.viewmodels.CurrentProjectViewModel;
 import org.odk.collect.android.activities.viewmodels.MainMenuViewModel;
 import org.odk.collect.android.activities.viewmodels.SplashScreenViewModel;
 import org.odk.collect.android.configure.qr.AppConfigurationGenerator;
@@ -14,15 +15,17 @@ import org.odk.collect.android.preferences.source.SettingsProvider;
 import org.odk.collect.android.projects.CurrentProjectProvider;
 import org.odk.collect.android.projects.ProjectCreator;
 import org.odk.collect.android.projects.SettingsConnectionMatcher;
+import org.odk.collect.android.utilities.ToastUtils;
 import org.odk.collect.projects.ProjectsRepository;
 
 import javax.inject.Inject;
 
+import butterknife.OnClick;
+
 public class CollectLauncherActivity extends AppCompatActivity {
 
-/*    @Inject
+    @Inject
     ProjectCreator projectCreator;
-
 
     @Inject
     ProjectsRepository projectsRepository;
@@ -36,43 +39,81 @@ public class CollectLauncherActivity extends AppCompatActivity {
     @Inject
     AppConfigurationGenerator appConfigurationGenerator;
 
-    @Inject
     SettingsConnectionMatcher settingsConnectionMatcher;
 
     @Inject
     SplashScreenViewModel.Factory splashScreenViewModelFactoryFactory;
 
-
-    SplashScreenViewModel viewModel;
-
     @Inject
-    MainMenuViewModel.Factory viewModelFactory;*/
+    CurrentProjectViewModel.Factory currentProjectViewModelFactory;
+
+    private SplashScreenViewModel viewModel;
+
+    private CurrentProjectViewModel currentProjectViewModel;
+
+
+
+
+    //button actions
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // DaggerUtils.getComponent(this).inject(this);
+        DaggerUtils.getComponent(this).inject(this);
         setContentView(R.layout.activity_odklauncher);
-       // viewModel = new ViewModelProvider(this, splashScreenViewModelFactoryFactory).get(ODKLauncher.class);
-       // settingsConnectionMatcher = new SettingsConnectionMatcher(projectsRepository, settingsProvider)
+
+        viewModel =  new ViewModelProvider(this, splashScreenViewModelFactoryFactory).get(SplashScreenViewModel.class);
+
+
+        DaggerUtils.getComponent(getApplicationContext()).inject(this);
+        settingsConnectionMatcher = new SettingsConnectionMatcher(projectsRepository, settingsProvider);
+        ToastUtils.showLongToast("Initiate with Launcher activity ");
+
 
         init();
     }
 
     private void init() {
-//        when {
-//            viewModel.shouldFirstLaunchScreenBeDisplayed -> {
-//                initializeLogin("https://kc.kobo.dreamsave.net/","ganidu", "UdjS%W5T" )
-//                //ActivityUtils.startActivityAndCloseAllOthers(this, FirstLaunchActivity::class.java)
-//            }
-//            viewModel.shouldDisplaySplashScreen -> startSplashScreen()
-//            else -> endSplashScreen()
-//        }
+
+        if (viewModel.getShouldFirstLaunchScreenBeDisplayed()){
+            initializeLogin("https://kc.kobo.dreamsave.net/","ganidu", "UdjS%W5T" );
+        }else{
+           // endSplashScreen();
+            currentProjectViewModel = new ViewModelProvider(this, currentProjectViewModelFactory).get(CurrentProjectViewModel.class);
+            currentProjectViewModel.getCurrentProject().observe(this, project -> {
+
+                ToastUtils.showLongToast("Already login to  " + project.getName());
+            });
+        }
     }
 
     private void endSplashScreen() {
         ActivityUtils.startActivityAndCloseAllOthers(this, MainMenuActivity.class);
     }
 
+    void initializeLogin( String url ,  String user ,  String pwd){
+        String settingsJson = appConfigurationGenerator.getAppConfigurationAsJsonWithServerDetails(
+                url,
+                user,
+                pwd
+        );
+
+       String UUID =  settingsConnectionMatcher.getProjectWithMatchingConnection(settingsJson);
+       if(UUID != null){
+           ToastUtils.showLongToast("Project alredy setup with " + UUID);
+
+       }else{
+           createProject(settingsJson);
+       }
+
+    }
+
+    void createProject( String settingsJson) {
+        projectCreator.createNewProject(settingsJson);
+        ActivityUtils.startActivityAndCloseAllOthers(this, MainMenuActivity.class);
+        ToastUtils.showLongToast(getString(R.string.switched_project, currentProjectProvider.getCurrentProject().getName()));
+    }
 
 }
