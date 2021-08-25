53b08b7df906098acb470bec8275da2d20c53f1a
Lakmal Rathnasiri <lakmal@calcey.com>
2021-08-25 21:38:31 +0530

    Add form download , open and upload functions

--- collect_app/src/main/java/org/odk/collect/android/activities/CollectLauncherActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/CollectLauncherActivity.java
@@ -13,14 +13,24 @@ import org.odk.collect.android.R;
 import org.odk.collect.android.activities.viewmodels.CurrentProjectViewModel;
 import org.odk.collect.android.activities.viewmodels.SplashScreenViewModel;
 import org.odk.collect.android.configure.qr.AppConfigurationGenerator;
+import org.odk.collect.android.external.FormsContract;
 import org.odk.collect.android.injection.DaggerUtils;
 import org.odk.collect.android.preferences.source.SettingsProvider;
 import org.odk.collect.android.projects.CurrentProjectProvider;
 import org.odk.collect.android.projects.ProjectCreator;
 import org.odk.collect.android.projects.SettingsConnectionMatcher;
 import org.odk.collect.android.utilities.ApplicationConstants;
+import org.odk.collect.android.utilities.FormsRepositoryProvider;
+import org.odk.collect.android.utilities.InstancesRepositoryProvider;
 import org.odk.collect.android.utilities.ToastUtils;
+import org.odk.collect.forms.Form;
+import org.odk.collect.forms.FormsRepository;
+import org.odk.collect.forms.instances.Instance;
 import org.odk.collect.projects.ProjectsRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -53,6 +63,12 @@ public class CollectLauncherActivity extends AppCompatActivity {
     @Inject
     CurrentProjectViewModel.Factory currentProjectViewModelFactory;
 
+    @Inject
+    FormsRepositoryProvider formsRepositoryProvider;
+
+    @Inject
+    InstancesRepositoryProvider instancesRepositoryProvider;
+
     private SplashScreenViewModel viewModel;
 
     private CurrentProjectViewModel currentProjectViewModel;
@@ -74,6 +90,15 @@ public class CollectLauncherActivity extends AppCompatActivity {
     @BindView(R.id.btn_upload_forms)
     Button btnUpload;
 
+
+
+    public String test_URL = "https://kc.kobo.dreamsave.net/";
+    public String test_user = "ganidu";
+    public String test_pwd = "UdjS%W5T";
+    //public String test_user = "asanka";
+    //public String test_pwd = "UwUD#Q8W";
+
+    public String test_form = "achbHHiVrNKvuhwqqfSNEX";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -111,22 +136,54 @@ public class CollectLauncherActivity extends AppCompatActivity {
     @OnClick({R.id.btn_get_forms})
     protected void downloadForms(){
 
-
+        String formId = test_form;
+        Intent i = new Intent(getApplicationContext(),
+                FormDownloadListActivity.class);
+        i.putExtra(ApplicationConstants.BundleKeys.FORM_IDS, new String[]{formId});
+        //i.putExtra(ApplicationConstants.BundleKeys.URL,"https://kc.kobo.dreamsave.net/");
+        startActivity(i);
     }
+
     @OnClick({R.id.btn_open_forms})
     protected void openForm(){
+//
+        long formdid = getFormId(test_form);
+        if (formdid != 0){
+            Uri formUri = FormsContract.getUri(currentProjectProvider.getCurrentProject().getUuid(), formdid );
+            Intent intent = new Intent(this, FormEntryActivity.class);
+            intent.setAction(Intent.ACTION_EDIT);
+            intent.setData(formUri);
+            intent.putExtra(ApplicationConstants.BundleKeys.FORM_MODE, ApplicationConstants.FormModes.EDIT_SAVED);
+            startActivity(intent);
+        }else{
+            ToastUtils.showLongToast("Form not found");
+        }
 
 
     }
+
     @OnClick({R.id.btn_upload_forms})
     protected void uploadForm(){
 
+        long instanceID = getInstanceId(test_form);
+
+        if (instanceID != 0){
+            long[] instanceIds = new long[]{instanceID};
+
+            Intent i = new Intent(this, InstanceUploaderActivity.class);
+            i.putExtra(FormEntryActivity.KEY_INSTANCES, instanceIds);
+            startActivity(i);
+        }else{
+            ToastUtils.showLongToast("Complete form not found for upload");
+        }
+
     }
 
+    //Login
     private void init() {
 
         if (viewModel.getShouldFirstLaunchScreenBeDisplayed()){
-            initializeLogin("https://kf.kobo.dreamsave.net/","asanka", "UwUD#Q8W" );
+            initializeLogin(test_URL,test_user, test_pwd );
         }else{
 
             currentProjectViewModel = new ViewModelProvider(this, currentProjectViewModelFactory).get(CurrentProjectViewModel.class);
@@ -159,8 +216,35 @@ public class CollectLauncherActivity extends AppCompatActivity {
 
     void createProject( String settingsJson) {
         projectCreator.createNewProject(settingsJson);
-        ActivityUtils.startActivityAndCloseAllOthers(this, MainMenuActivity.class);
+       // ActivityUtils.startActivityAndCloseAllOthers(this, MainMenuActivity.class);
         ToastUtils.showLongToast(getString(R.string.switched_project, currentProjectProvider.getCurrentProject().getName()));
+    }
+
+
+    //Get form id
+    private long getFormId(String id){
+        List<Form> formsFromDB = formsRepositoryProvider.get().getAll();
+        if(formsFromDB != null ){
+            for (Form form : formsFromDB){
+                if(form.getFormId().equals(id)){
+                    return form.getDbId();
+                }
+            }
+        }
+        return 0;
+    }
+
+    //Get completed form id from instance table
+    private long getInstanceId(String id){
+        List<Instance> instances = instancesRepositoryProvider.get().getAll();
+        if(instances != null ){
+            for (Instance instance : instances){
+                if(instance.getFormId().equals(id)){
+                    return instance.getDbId();
+                }
+            }
+        }
+        return 0;
     }
 
 }
